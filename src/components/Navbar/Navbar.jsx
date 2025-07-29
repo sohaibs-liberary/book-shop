920b3828b0d504dedaa790981105746aa5697751
Sohaib-alt <muhammadsohaibshakeel18@gmail.com>
2025-07-30 00:08:14 +0500

    navbar logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        <div className="container">
+            <div className="flex justify-between items-center">
+                <div>
+                    <a href='#'>
+                        <img src="https://bookstore-tcj.netlify.app/assets/logo-yh4yOaug.png" alt="" className='w-20' />
+                    </a>
+                    Books
+                </div>
+                <div>
+                    <ul>
+                        <li>Home</li>
+                        <li>Best Seller</li>
+                        <li>Home</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
