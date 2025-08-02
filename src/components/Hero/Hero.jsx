09d672e7d50012c6974fc2ebfd3f65c03ea7c09e
Sohaib-alt <muhammadsohaibshakeel18@gmail.com>
2025-08-02 23:41:18 +0500

    change in books

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useState , useEffect} from 'react'
+import React, { useState , useEffect  } from 'react'
 import Book1 from "../../assets/images/book2-C96QbsXx.jpg"
 import Book2 from "../../assets/images/book1-T1QIvOvc.jpg"
 import Book3 from "../../assets/images/book3-5_h1HoKi.jpg"
@@ -6,7 +6,10 @@ import Vector from "../../assets/images/blue-pattern-frndFZgs.png"
 
 const Hero = () => {
    
-    const[imageid , setimageid] = useState(Book1)
+    const[imageid , setImageid] = useState(Book1);
+    const[tital , settital] =useState(" His Life will forever be Changed ");
+    const[description , setDescription]=useState(" His Whole Life by Elizabeth Hay is a coming-of-age story set in the mid-1990s following a young boy named Jim as his family grapples with a faltering marriage and the political tensions surrounding Quebecs potential separation from Canada.");
+
 
 
   return (
@@ -17,12 +20,12 @@ const Hero = () => {
             {/*text-content-section*/}
             <div className='px-10'>
                 <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold ' >
-                    His Life will forever be Changed
+                  {tital}
                     <p className='bg-clip-text text-transparent bg-gradient-to-r from-pink-600 to-purple-500 text-right  text-sm '  > by Anonymous</p>
 
                 </h1>
                 <p className='text-sm' >
-                  "His Whole Life" by Elizabeth Hay is a coming-of-age story set in the mid-1990s, following a young boy named Jim as his family grapples with a faltering marriage and the political tensions surrounding Quebec's potential separation from Canada."
+                {description}
                 </p>
                 <div className=''>
                     <button className='bg-gradient-to-r from-purple-700 to-pink-400 text-white py-3  px-7 rounded-full my-3 text-xl hover:scale-105 duration-200 mt-4 '>Order Now</button>
