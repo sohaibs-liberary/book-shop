14bc3eaed03894cd028e65e35ae8ec8dd2b162ba
Sohaib-alt <muhammadsohaibshakeel18@gmail.com>
2025-08-06 00:03:03 +0500

    Bset Books Section Start

--- src/components/BestBooks.js/BestBooks.jsx
+++ src/components/BestBooks.js/BestBooks.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import Book1 from "../../assets/images/book2-C96QbsXx.jpg"
+import Book2 from "../../assets/images/book1-T1QIvOvc.jpg"
+import Book3 from "../../assets/images/book3-5_h1HoKi.jpg"
+
 
 const BestBooks = () => {
+  const BooksData =[
+          {
+              id:1,
+              img:Book1,
+              tital: "His Life will forever be Changed",
+              description: "His Whole Life by Elizabeth Hay is a coming-of-age story set in the mid-1990s following a young boy named Jim as his family grapples with a faltering marriage and the political tensions surrounding Quebecs potential separation from Canada.",
+          },
+          {
+              id:2,
+              img:Book2,
+              tital: "Who's there",
+              description: "Who's There? by Stephanie S. Tolan is a young adult novel about two orphaned siblings, Drew and Evan Broderick, who move into their late father's childhood home, Rose Hill, after their parents die in a gas explosion.",
+          },
+          {
+              id:3,
+              img:Book3,
+              tital: "Lost Boy",
+              description: "Lost Boy by Christina Henry offers a dark, gritty retelling of the Peter Pan story, focusing on the relationship between Peter and his first Lost Boy, Jamie, before the arrival of Wendy.",
+          },
+      ]
   return (
-    <div>BestBooks</div>
+    <>
+      <div className=" dark:bg-gray-900 dark:text-white py-10">
+        <div className="container ">
+          <div className='text-center mb-20 max-w-[400px] mx-auto '>
+            <p className="text-sm bg-clip-text text-transparent bg-gradient-to-r from-purple-800 to-purple-400  font-semibold ">Trending Books</p>
+            <h1 className='font-extrabold text-5xl'>Best Books</h1>
+            <p className="text-gray-500  text-xs py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, minima eligendi?</p>
+          </div>
+          {/*Cards Sections*/}
+          <div className='grid grid-cols-1 sm:grid-cols-2 md-grid-cols-3 md:gap-5  place-items-center'  >
+            {/*Card-1*/}
+            {
+              BooksData.map((books) =>(
+
+                <div className=""> </div>
+             
+              ))
+            }
+          </div>
+        </div>
+      </div>
+
+    </>
   )
 }
 
-export default BestBooks
+export default BestBooks;
