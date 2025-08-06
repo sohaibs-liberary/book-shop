538b49c828cac43aa017e7c18347a646679a55aa
Sohaib-alt <muhammadsohaibshakeel18@gmail.com>
2025-08-06 12:09:47 +0500

    besyt bookas image set

--- src/components/BestBooks.js/BestBooks.jsx
+++ src/components/BestBooks.js/BestBooks.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
-import Book1 from "../../assets/images/book2-C96QbsXx.jpg"
-import Book2 from "../../assets/images/book1-T1QIvOvc.jpg"
-import Book3 from "../../assets/images/book3-5_h1HoKi.jpg"
-
+import Img1 from "../../assets/images/book2-C96QbsXx.jpg"
+import Img2 from "../../assets/images/book1-T1QIvOvc.jpg"
+import Img3 from "../../assets/images/book3-5_h1HoKi.jpg"
+import { FaStar } from "react-icons/fa";
 
 const BestBooks = () => {
-  const BooksData =[
-          {
-              id:1,
-              img:Book1,
-              tital: "His Life will forever be Changed",
-              description: "His Whole Life by Elizabeth Hay is a coming-of-age story set in the mid-1990s following a young boy named Jim as his family grapples with a faltering marriage and the political tensions surrounding Quebecs potential separation from Canada.",
-          },
-          {
-              id:2,
-              img:Book2,
-              tital: "Who's there",
-              description: "Who's There? by Stephanie S. Tolan is a young adult novel about two orphaned siblings, Drew and Evan Broderick, who move into their late father's childhood home, Rose Hill, after their parents die in a gas explosion.",
-          },
-          {
-              id:3,
-              img:Book3,
-              tital: "Lost Boy",
-              description: "Lost Boy by Christina Henry offers a dark, gritty retelling of the Peter Pan story, focusing on the relationship between Peter and his first Lost Boy, Jamie, before the arrival of Wendy.",
-          },
-      ]
+  const BooksData = [
+    {
+      id: 1,
+      img: Img1,
+      tital: "His Life will forever be Changed",
+      description: "His Whole Life by Elizabeth Hay is a coming-of-age story set in the mid-1990s following a young boy named Jim as his family grapples with a faltering marriage and the political tensions surrounding Quebecs potential separation from Canada.",
+    },
+    {
+      id: 2,
+      img: Img2,
+      tital: "Who's there",
+      description: "Who's There? by Stephanie S. Tolan is a young adult novel about two orphaned siblings, Drew and Evan Broderick, who move into their late father's childhood home, Rose Hill, after their parents die in a gas explosion.",
+    },
+    {
+      id: 3,
+      img: Img3,
+      tital: "Lost Boy",
+      description: "Lost Boy by Christina Henry offers a dark, gritty .",
+    },
+  ]
   return (
     <>
       <div className=" dark:bg-gray-900 dark:text-white py-10">
@@ -35,13 +35,35 @@ const BestBooks = () => {
             <p className="text-gray-500  text-xs py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, minima eligendi?</p>
           </div>
           {/*Cards Sections*/}
-          <div className='grid grid-cols-1 sm:grid-cols-2 md-grid-cols-3 md:gap-5  place-items-center'  >
+          <div className='grid grid-cols-1 lg:flex lg:justify-around sm:grid-cols-2 md-grid-cols-3 md:gap-5  place-items-center'  >
             {/*Card-1*/}
             {
-              BooksData.map((books) =>(
+              BooksData.map((books) => (
 
-                <div className=""> </div>
-             
+                <div className=''>
+
+                  <div className="image text-center px-10">
+                    <img src={books.img} alt="" className='w-40 h-60 ' />
+                  <div className="text">
+                    <div className="flex w-full font-extrabold items-center justify-center py-3">
+                      <FaStar
+                        className='text-yellow-500 ' />
+                      <FaStar
+                        className='text-yellow-500' />
+                      <FaStar
+                        className='text-yellow-500' />
+                      <FaStar
+                        className='text-yellow-500' />
+
+                    </div>
+
+                  </div>
+                    <h1 className='text-center '>{books.tital}</h1>
+                    <p className='text-center'>{books.description}</p>
+                    <button className='bg-gradient-to-r from-purple-700 to-pink-500  text-white py-2 px-8  font-semibold rounded-full'>Order Now</button>
+                  </div>
+                </div>
+
               ))
             }
           </div>
